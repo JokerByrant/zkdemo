9c0881a0312c78de13f9993bddc9b93958bc2548
sxhjoker <sxhjoker@163.com>
2021-08-02 09:44:36 +0800

    回调事件

--- src/main/java/com/study/demo/zk/ZKAsyncDemo.java
+++ src/main/java/com/study/demo/zk/ZKAsyncDemo.java
@@ -1,18 +1,15 @@
 package com.study.demo.zk;
 
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+import org.apache.zookeeper.ZooDefs.Ids;
+
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
-import org.apache.zookeeper.AsyncCallback;
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.Watcher.Event.KeeperState;
-import org.apache.zookeeper.ZooDefs.Ids;
-import org.apache.zookeeper.ZooKeeper;
-
-//连接后创建回调
+/**
+ * 创建节点并完成回调
+ */
 class IStringCallback implements AsyncCallback.StringCallback {
 	public void processResult(int rc, String path, Object ctx, String name) {
 		System.out.println("create path result: [" + rc + ", " + path + "," + ctx + ", real path name: " + name);
@@ -23,7 +20,7 @@ public class ZKAsyncDemo implements Watcher {
 	private static final CountDownLatch cdl = new CountDownLatch(1);
 
 	public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
-		ZooKeeper zk = new ZooKeeper("192.168.152.130:2181", 5000, new ZKAsyncDemo());
+		ZooKeeper zk = new ZooKeeper("localhost:2181", 5000, new ZKAsyncDemo());
 		cdl.await();
 
 		zk.create("/zk-test-", "123".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, new IStringCallback(),
--- src/main/java/com/study/demo/zk/ZKChildrenAsyncDemo.java
+++ src/main/java/com/study/demo/zk/ZKChildrenAsyncDemo.java
@@ -1,23 +1,21 @@
 package com.study.demo.zk;
 
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.data.Stat;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
-import org.apache.zookeeper.AsyncCallback;
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.Watcher.Event.EventType;
-import org.apache.zookeeper.Watcher.Event.KeeperState;
-import org.apache.zookeeper.ZooDefs.Ids;
-import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.data.Stat;
-
-//异步调用并完成回调
+/**
+ * 创建子节点：异步调用并完成回调
+ * 相对于监听器，回调事件似乎更有针对性，监听器的生效还需要在对应操作前完成注册监听器的操作，回调事件直接在操作时将对应的回调事件传入即可。
+ */
 class ChildrenCallback implements AsyncCallback.Children2Callback {
-	
+
 	public void processResult(int rc, String path, Object ctx, List<String> children, Stat stat) {
 		System.out.println(
 				"Child: " + rc + ", path: " + path + ", ctx: " + ctx + ", children: " + children + ", stat: " + stat);
@@ -29,19 +27,25 @@ public class ZKChildrenAsyncDemo implements Watcher {
 	private static ZooKeeper zk = null;
 
 	public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
-		zk = new ZooKeeper("192.168.152.130:2181", 5000, new ZKChildrenAsyncDemo());
+		zk = new ZooKeeper("localhost:2181", 5000, new ZKChildrenAsyncDemo());
 		cdl.await();
 
-		zk.create("/zk-test", "123".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		if (zk.exists("/zk-test", true) == null) {
+			zk.create("/zk-test", "123".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		}
 
+		// 第一次创建子节点，不会触发监听器
 		zk.create("/zk-test/c1", "456".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
 
 		zk.getChildren("/zk-test", true, new ChildrenCallback(), "ok");
 
+		// 第二次创建子节点，触发监听器
+		zk.create("/zk-test/c2", "789".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+
 		Thread.sleep(Integer.MAX_VALUE);
 	}
 
-	
+
 	//监听到事件时进行处理
 	public void process(WatchedEvent event) {
 		if (KeeperState.SyncConnected == event.getState())
